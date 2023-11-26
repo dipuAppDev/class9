f373609abaf1fc50a863f35faa1bb7be965c0995
dipuAppDev <dipuappdev@gmail.com>
2023-11-26 09:00:04 +0600

    file updated

---
+++ class9.kt
@@ -0,0 +1,89 @@
+/**
+ * while loop
+ */
+fun main(args:Array<String>){
+    /*  var i = 0
+      while (i<10){
+          println(i)
+          i++
+      }*/
+
+    /**
+     * add natural number (0-9)
+     */
+//    var i = 0
+//    var sum = 0
+//    while (i<10){
+//        sum+=i // sum = sum+i // sum=0+0->sum=0+1->sum=1+2.....
+//        i++
+//    }
+//    println(sum)
+
+//    var i = 9
+//    var sum = 0
+//    while (i>0){ // while(i>-1)
+//       sum+=i // sum = sum+i // sum=0+0->sum=0+1->sum=1+2.....
+//       i--
+//    }
+//    println(sum)
+
+    /**
+     * do-while loop
+     */
+//    var i = 0
+//    var sum = 0
+//    do{
+//        sum=sum+i
+//        i++
+//    }while(i<10)
+//        println(sum)
+
+    /**
+     * for loop
+     */
+
+//    var sum = 0
+//    for (i in 0..9){
+//        sum+=i
+//    }
+//    println(sum)
+
+    /**
+     * class work (print even number from 1-20)
+     */
+//    print("Even numbers from 1-20 is = ")
+//    for(i in 1..20){ //range number from 1-20
+//        if (i%2==0){
+//            //print("Even number is from = ")
+//            print("${i} ")
+//        }
+//    }
+    /**
+     * break statement
+     */
+//    for (i in 1..20){
+//        //println(i) // right
+//        if (i==5){
+//            break
+//        }
+//        //println(i)->error
+//
+//    }
+    /**
+     * continue statement
+     */
+
+
+//    for (i in 1..20){
+//        //println(i)->error
+//        if (i==5){
+//            continue
+//        }
+//        println(i) // right
+//    }
+
+
+
+
+
+}
